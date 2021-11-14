8b40be74613994858ea83581ba67141f06c6e577
rj <rjahrj@gmail.com>
2021-11-14 17:07:15 -0500

    fixed autocmds

--- lua/basics.lua
+++ lua/basics.lua
@@ -35,3 +35,44 @@ vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
 vim.o.scrolloff = 10
+
+--Autochange
+vim.o.autochdir = true
+
+-- autocommands
+--- This function is taken from https://github.com/norcalli/nvim_utils
+--function nvim_create_augroups(definitions)
+--  for group_name, definition in pairs(definitions) do
+--    api.nvim_command('augroup '..group_name)
+--    api.nvim_command('autocmd!')
+--    for _, def in ipairs(definition) do
+--      local command = table.concat(vim.tbl_flatten{'autocmd', def}, ' ')
+--      api.nvim_command(command)
+--    end
+--    api.nvim_command('augroup END')
+--  end
+--end
+--
+--local autocmds = {
+--    reload_vimrc = {
+--        -- Reload vim config automatically
+--        {"BufWritePost",[[$VIM_PATH/{*.vim,*.yaml,vimrc} nested source $MYVIMRC | redraw]]};
+--    };
+--    packer = {
+--        { "BufWritePost", "plugins.lua", "PackerCompile" };
+--    };
+--    terminal_job = {
+--        { "TermOpen", "*", "startinsert" };
+--        { "TermOpen", "*", "setlocal listchars= nonumber norelativenumber" };
+--    };
+--    resize_windows_proportionally = {
+--        { "VimResized", "*", ":wincmd =" };
+--    };
+--    toggle_search_highlighting = {
+--        { "InsertEnter", "*", "setlocal nohlsearch" };
+--    };
+--}
+--
+--nvim_create_augroups(autocmds)
+-- autocommands END
+
